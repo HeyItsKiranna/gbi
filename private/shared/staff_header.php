c20532bd2c79fa945cfb24e44613f70116f1ada1
Owner <Christiancstepniak@students.abtech.edu>
2021-03-25 11:03:36 -0400

    Finished chapter 1 of the globe_bank tutorial. Next is chapter 2: Build Web Pages with PHP

---
+++ private/shared/staff_header.php
@@ -0,0 +1,18 @@
+<?php
+if(!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>GBI - <?php echo $page_title; ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+  </head>
+
+  <body>
+    <header><h1>GBI Staff Area</h1></header>
+    
+    <nav><ul>
+      <li><a href="index.php">Menu</a></li>
+      </ul>
+    </nav>
---
+++ public/staff/index.php
@@ -0,0 +1,10 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<?php $page_title = 'Staff menu'; ?>
+
+<div id="content">
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
