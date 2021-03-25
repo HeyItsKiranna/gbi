8a345c7e40270f5ae873f7df275eecb12b3066d7
Owner <Christiancstepniak@students.abtech.edu>
2021-03-25 13:56:49 -0400

    Finished chapter 2 of the globe_bank tutorial, next is chapter 3

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -6,13 +6,13 @@ if(!isset($page_title)) { $page_title = 'Staff Area'; }
   <head>
     <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../stylesheets/staff.css">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css') ?>">
   </head>
 
   <body>
     <header><h1>GBI Staff Area</h1></header>
     
     <nav><ul>
-      <li><a href="index.php">Menu</a></li>
+      <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
       </ul>
     </nav>
--- public/staff/pages/index.php
+++ public/staff/pages/index.php
@@ -1,12 +1,25 @@
+<?php
+if(!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
 <!doctype html>
-
 <html lang="en">
   <head>
-    <title></title>
+    <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css') ?>">
   </head>
 
   <body>
+    <header><h1>GBI Staff Area</h1></header>
+    
+    <nav><ul>
+      <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
+      </ul>
+    </nav>
 
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank 
+    </footer>
   </body>
 </html>
